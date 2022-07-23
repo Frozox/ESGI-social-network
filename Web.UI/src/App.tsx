873bcf70da32f38f862c1a24f5551be1eaad25f3
Naty Ghanem <naty.ghanem@idcapture.fr>
2022-07-23 22:31:45 +0200

    v2 sideBar

--- Web.UI/src/App.tsx
+++ Web.UI/src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import MyRoutes from './routes/routes'
-import SideNav from "./components/SideBar";
+import SideBar from "./components/SideBar";
+import { useLocation} from 'react-router-dom';
 function App() {
+  const location = useLocation();
   return (
     <div className="flex">
-      <SideNav />
-      <MyRoutes />
+      <SideBar location={location}>
+        <MyRoutes />
+      </SideBar>
     </div>
   )
 }
--- Web.UI/src/components/SideBar.tsx
+++ Web.UI/src/components/SideBar.tsx
@@ -1,9 +1,12 @@
-import { ChatIcon, CogIcon, HomeIcon, TemplateIcon } from "@heroicons/react/outline";
-import { NavLink } from 'react-router-dom';
-import { useState, useEffect } from "react";
+import { ChatIcon, CogIcon, HomeIcon, TemplateIcon, LogoutIcon } from "@heroicons/react/outline";
+import { NavLink, Path } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
 import { whenPatternMatches } from "../utils/helpers/index";
-
-const SideBar = () => {
+interface navBarProps {
+  children: React.ReactNode,
+  location: Path
+}
+const SideBar = ({ children, location }: navBarProps) => {
   const [display, setDisplay] = useState(true)
 
   useEffect(() => {
@@ -13,52 +16,78 @@ const SideBar = () => {
     ])
 
     return () => {
-        setDisplay(true)
+      setDisplay(true)
     }
-}, [location.pathname])
+  }, [location.pathname])
 
+  const NavItem = ({ link, name, icon }: any) => {
+    const active = location.pathname === link;
+    return (
+      <NavLink to={link} className={`${active ? 'bg-gray-100 text-gray-900' : ''} flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out`} data-mdb-ripple="true" data-mdb-ripple-color="dark">
+        <span className="h-5 w-5 mr-1" >{icon}</span>
+        <span>{name}</span>
+      </NavLink>
+    )
+  }
+  const user = {
+    firstName: "John",
+    profilePicture: "https://randomuser.me/api/portraits/lego/1.jpg",
+  }
   return (
     <>
-    {!display ? ('') : (
-    <aside className="w-44 left-0 top-0 h-screen bg-white-700">
-      <div className='max-w-[250px] border-r relative flex flex-col py-10 min-h-screen group' >
-        <div className='grow'>
-          <div className='my-2 ' >
-            <div className='flex px-4 py-1 cursor-pointer' >
-              <HomeIcon className="h-5 w-5 text-black-500" />
-              <p className='mb-2 ml-2 font-bold text-black-500' >Home</p>
+      {!display ? ('') : (
+        <div className="w-44 left-0 top-0 h-screen relative">
+          <aside className="w-full left-0 top-0 h-screen bg-white-700 fixed absolute">
+            <div className='max-w-[250px] border-r relative flex flex-col py-10 min-h-screen group' >
+              <div className='grow'>
+                <div className='my-2 mb-6' >
+                  <div className='flex px-4 py-1 cursor-pointer items-center' >
+                    <img className="object-cover w-10 h-10 rounded-full"
+                      src={user.profilePicture}
+                      alt="username" />
+                    <span className="block ml-2 font-bold text-black-600">{user.firstName}</span>
+
+                  </div>
+                </div>
+                <div className='my-2 ' >
+                  <div className='flex px-4 py-1 cursor-pointer' >
+                    <HomeIcon className="h-5 w-5 text-black-500" />
+                    <p className='mb-2 ml-2 font-bold text-black-500' >Home</p>
+                  </div>
+                  <ul className="relative">
+                    <li className="relative">
+                      <NavItem link="/chat" icon={<ChatIcon />} name="Chat" />
+                    </li>
+                  </ul>
+                </div>
+                <div className='my-2' >
+                  <div className='flex px-4 py-1 ' >
+                    <TemplateIcon className="h-5 w-5 text-black-500" />
+                    <p className='mb-2 ml-2 font-bold text-black-500' >Dashboard</p>
+                  </div>
+                  <ul className="relative">
+                    <li className="relative">
+                      <NavItem link="#!" icon={<CogIcon />} name="Settings" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div>
+                <div className='my-2' >
+                  <ul className="relative">
+                    <li className="relative">
+                      <NavItem link="/logout" icon={<LogoutIcon />} name="Logout"/>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <ul className="relative">
-              <li className="relative">
-                <NavLink to="/chat" className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="dark">
-                    <ChatIcon className="h-5 w-5 text-gray-500 mr-1" ></ChatIcon>
-                    <span>Chat</span>
-                </NavLink>
-              </li>
-            </ul>
-          </div>
+          </aside>
         </div>
-        <div>
-          <div className='my-2' >
-            <div className='flex px-4 py-1 ' >
-              <TemplateIcon className="h-5 w-5 text-black-500" />
-              <p className='mb-2 ml-2 font-bold text-black-500' >Dashboard</p>
-            </div>
-            <ul className="relative">
-              <li className="relative">
-                <a className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="dark">
-                  <CogIcon className="h-5 w-5 text-gray-500 mr-1" />
-                  <span>Settings</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </aside>
-    )}
+      )}
+      {children}
     </>
-  );
+  )
 }
 
 export default SideBar;
